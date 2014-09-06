3ae6f72812fa818ce111d683baa1e4ddb104ec9c
Alyssa Carter <trillionalyssa@gmail.com>
2014-09-06 09:42:57 -0700

    Set initial conditions to a single glider

--- main.c
+++ main.c
@@ -22,8 +22,15 @@ void print_life(life l) {
   putchar('\n');
 }
 
+
+// 0 0 0 0
+// 0 0 1 0
+// 0 0 0 1
+// 0 1 1 1
+region gliderRegion = 59456;//2^15 + 2^14 + 2^13 + 2^11 + 2^6;
+
 void main(){
-  region r[4] = {54387, 34298, 1290, 17};
+  region r[4] = {gliderRegion, 0, 0, 0};
   life l = { .w = 8, .h = 8, .regions = r };
 
   print_life(l);
@@ -32,8 +39,8 @@ void main(){
     c = getchar();
     if (c == 'q') break;
     else if (c == 'r') {
-      l.regions[0] = 54387; l.regions[1] = 34298;
-      l.regions[2] = 1290; l.regions[3] = 17;
+      l.regions[0] = gliderRegion; l.regions[1] = 0;
+      l.regions[2] = 0; l.regions[3] = 0;
     }
     else if (c == 's') {
       cornerRef dst = { &l.regions[0], &l.regions[1], &l.regions[2], &l.regions[3] };
